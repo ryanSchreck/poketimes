9d0a8fc08b21cbb1148ffe0af4f64ff23340aa81
Ryan Schreck <ryan.schreck28@gmail.com>
2020-09-06 13:08:25 -0700

    feat(poketimes): basic blog app w/ API inspired by The Net Ninja

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Navbar from './components/navbar';
+import Home from './components/home';
+import About from './components/about';
+import Contact from './components/contact';
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import Post from './components/post'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="App">
+          <Navbar />
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route path='/about' component={About} />
+            <Route path='/contact' component={Contact} />
+            <Route path="/:post_id" component={Post} />
+          </Switch>
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/about.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Rainbow from '../hoc/rainbow'
+
+const About = () => {
+  return (
+    <div className="container">
+      <h4 className="center">About</h4>
+      <p>yada yadda yadda</p>
+    </div>
+  )
+}
+
+export default Rainbow(About);
